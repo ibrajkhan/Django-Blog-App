9ddc6cbeef9e9700483112f2ee3449019b044c8d
IbrajKhan <ibraj.grd@gmail.com>
2020-07-09 20:51:32 +0530

    url mapping to view function

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('<h1>Blog post with Mark<h1>')
+
+
+def about(request):
+    return HttpResponse('<h1>Blog About<h1>')
+
+
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!h:\django\django _proje\blogapp\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
