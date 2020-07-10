a3e969ad3dc37f8a7150ba1cfa4654905af91906
IbrajKhan <ibraj.grd@gmail.com>
2020-07-10 21:40:46 +0530

    Added Templates

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Ibraj khan',
+        'title': 'Blog Post 1',
+        'content': 'The life change',
+        'date_of_post': 'June 6 , 2020'
+    },
+
+    {
+        'author': 'Mumtaz khan',
+        'title': 'Blog Post 2',
+        'content': 'HTML5',
+        'date_of_post': 'June 10 , 2020'
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog post with Mark<h1>')
+    return render(request, 'blog/home.html', {'posts': posts})
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About<h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
 
 
